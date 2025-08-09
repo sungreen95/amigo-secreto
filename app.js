f9b4b3af6156a98b90f6c17d82e266307e4c1703
sungreen95 <dsantiagox95@hotmail.comm>
2025-08-08 20:10:24 -0500

    creando juego

---
+++ app.js
@@ -0,0 +1,15 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos =[];
+consolelog ("nombres")
+
+function adicionAmigo() {
+    const input =document.getElementById("nombre");
+    const nombre = input.value.trim();
+
+    if(nombre==""){
+        alert("por favor, escribe un nombre valido para el juego");
+        return;
+
+    }
+}
+
