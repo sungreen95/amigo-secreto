9428244032c0b3e7f9fe257794878d5135cd58ff
sungreen95 <dsantiagox95@hotmail.comm>
2025-08-09 10:26:10 -0500

    actualización 3

--- app.js
+++ app.js
@@ -27,3 +27,33 @@ function mostrarlista(){
 
     });
 }
+function mostrarlista() {
+    const lista= document.getElementById("listaAmigos");
+    lista.innerHTML ="";
+
+    amigos.forEach(amigo => {
+        const li = document. createElement(li);
+        li.textContent = amigo;
+        lista.appendChild(li);
+
+    });
+}
+
+function sorteaAmigo(){
+    const resultado= document.getElementById("resultado");
+    resultado.innerHTML="";
+
+    if (amigos.length=== 0){
+        alert("no hay nombres en la lista para sortear");
+        return;
+    }
+
+    const indicealeatorio = Math.floor(Math.random() + amigos.length);
+    const amigoSecreto = amigo[ indicealeatorio];
+
+    const li = document.createElement("li");
+    li.textContent = `🎉 El amigo secreto es: ${amigoSecreto} 🎉`;
+    resultado.appendChild(li);
+
+
+}
