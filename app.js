d46e556075786430161e8503cce837a04576eb56
sungreen95 <dsantiagox95@hotmail.comm>
2025-08-09 10:53:04 -0500

    actualización 4

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos =[];
-consolelog ("nombres")
 
 function adicionAmigo() {
     const input =document.getElementById("nombre");
